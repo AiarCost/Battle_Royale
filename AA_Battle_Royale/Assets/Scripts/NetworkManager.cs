46f32a4a4a4c23e5ec4529cf2f8fc5838a04dca0
AiarCost <65986842+AiarCost@users.noreply.github.com>
2021-09-16 16:59:19 -0700

    Build V01 & project import

---
+++ AA_Battle_Royale/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+
+    // instance
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We've connected to the master server!");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    // attempts to create a room
+    public void CreateRoom( string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    // attempts to join a room
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
